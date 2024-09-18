6b5b3eff03501d590fb9e065c074e253ced15ab4
Tolga <123948207+tolgapp@users.noreply.github.com>
2024-09-18 21:24:23 +0200

    Favorite page scrollingtext added and some styling changes

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -2,33 +2,26 @@ import React from "react";
 import products from "../data/products.json";
 import ProductCard from "../components/ProductCard";
 import "../style/Favorites.css";
+import ScrollingText from "../components/ScrollingText";
 
 type FavoriteProps = {
   favoriteProducts: number[];
-  isLoggedIn: boolean
+  isLoggedIn: boolean;
   onToggleFavorite: (productId: number) => void;
 };
 
 const Favorites: React.FC<FavoriteProps> = ({
   favoriteProducts,
   onToggleFavorite,
-  isLoggedIn
+  isLoggedIn,
 }) => {
   const favProducts = products.filter((product) =>
     favoriteProducts.includes(product.id)
   );
 
-  const containerStyle: React.CSSProperties = {
-    minHeight: favProducts.length === 0 ? "45rem" : "auto",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: favProducts.length === 0 ? "center" : "flex-start",
-  };
-
   return (
-    <div style={containerStyle} className="favorites-container">
-      <h1>Favorites Page</h1>
+    <div className="favorites-container">
+      <ScrollingText text={"Your favorites"} />
       {favProducts.length === 0 ? (
         <p>No favorites selected.</p>
       ) : (
